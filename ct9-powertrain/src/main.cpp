a15d92b4e12882b99dd7cb0e8071a86164365920
lmoulin <lmoulin@spartan-conseil.fr>
2021-04-08 18:40:54 +0200

    make build in led blinking

--- ct9-powertrain/src/main.cpp
+++ ct9-powertrain/src/main.cpp
@@ -1,9 +1,39 @@
 #include <Arduino.h>
 
+// Variables will change:
+int ledState = LOW; // ledState used to set the LED
+
+// Generally, you should use "unsigned long" for variables that hold time
+// The value will quickly become too large for an int to store
+unsigned long previousMillis = 0; // will store last time LED was updated
+
+// constants won't change:
+const long interval = 1000; // interval at which to blink (milliseconds)
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    unsigned long currentMillis = millis();
+
+    if (currentMillis - previousMillis >= interval)
+    {
+        // save the last time you blinked the LED
+        previousMillis = currentMillis;
+
+        // if the LED is off turn it on and vice-versa:
+        if (ledState == LOW)
+        {
+            ledState = HIGH;
+        }
+        else
+        {
+            ledState = LOW;
+        }
+
+        // set the LED with the ledState of the variable:
+        digitalWrite(LED_BUILTIN, ledState);
+    }
 }
