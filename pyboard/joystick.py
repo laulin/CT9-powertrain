fdcd42495d6c4b743682b7b83a59dafe0f18db29
laulin <laulin@nomail.com>
2021-12-28 15:06:00 +0100

    add joystick class

--- pyboard/joystick.py
+++ pyboard/joystick.py
@@ -1,3 +1,9 @@
+try:
+    import machine
+except:
+    print("Bypass machine import")
+
+
 def transform(forward:bool, reverse:bool, left:bool, right:bool):
     if left and right:
         raise Exception("Left and right conflict")
@@ -21,3 +27,13 @@ def transform(forward:bool, reverse:bool, left:bool, right:bool):
             return -1,0 
     else:
         return 0,0
+
+class Joystick:
+    def __init__(self, forward_pin, reverse_pin, left_pin, right_pin):
+        self._forward_pin = machine.Pin(forward_pin, machine.Pin.OUT)
+        self._reverse_pin = machine.Pin(reverse_pin, machine.Pin.OUT)
+        self._left_pin = machine.Pin(left_pin, machine.Pin.OUT)
+        self._right_pin = machine.Pin(right_pin, machine.Pin.OUT)
+
+    def get(self):
+        return transform(self._forward_pin.value, self._reverse_pin.value, self._left_pin.value, self._right_pin.value)
