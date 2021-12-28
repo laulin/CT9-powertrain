f79db647882388e212fcfbb84363a0bf42b5412e
laulin <laulin@nomail.com>
2021-12-28 15:29:21 +0100

    add motor controler

---
+++ pyboard/motor_controler.py
@@ -0,0 +1,27 @@
+import machine
+
+class MotorControler:
+    def __init__(self, pwm_pin, a_pin, b_pin) -> None:
+        self._pwm_pin = machine.Pin(pwm_pin)
+        self._pwm = machine.PWM(self._pwm_pin)
+        self._pwm.duty(0)
+
+        self._a_pin = machine.Pin(a_pin, machine.Pin.OUT)
+        self._a_pin.off()
+
+        self._b_pin = machine.Pin(b_pin, machine.Pin.OUT)
+        self._b_pin.off()
+
+    def set(self, value):
+        if value > 0:
+            self._a_pin.on()
+            self._b_pin.off()
+            self._pwm.duty(value)
+        elif value < 0:
+            self._a_pin.off()
+            self._b_pin.on()
+            self._pwm.duty(-value)
+        else:
+            self._a_pin.off()
+            self._b_pin.of()
+            self._pwm.duty(0)
